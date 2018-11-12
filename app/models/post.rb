17c2b5dc7957048ef255bd92726ce63b07b4df42
Taylor Williams <taylor@thewilliams.net>
2018-11-12 02:27:52 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   validates :title, presence: :true
-  validates :category, includes: {in: %w(Fiction Non-Fiction)}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
